aa317f5293f3b0549b312110abf10c07b03a0175
bombk1n <grinenkoroman2110@gmail.com>
2024-11-13 18:53:53 +0200

    create project

---
+++ src/main/java/com/bombk1n/materialpro/MaterialProApplication.java
@@ -0,0 +1,13 @@
+package com.bombk1n.materialpro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MaterialProApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MaterialProApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/bombk1n/materialpro/MaterialProApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bombk1n.materialpro;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MaterialProApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
